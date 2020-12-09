817a5e5e4d428a1dd284d3681c3b2ac07e447d3f
richardykedang <kedangrichard@gmail.com>
2020-12-09 16:26:36 +0700

    Install dotenv and setting database config

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = require('./app');
 
-const PORT = 3000
+dotenv.config({ path:'./config.env'});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD);
+mongoose.connect(DB,{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}).then(() => console.log("DB connection successful!"));
+
+const PORT = process.env.PORT||3000;
 app.listen(3000, ()=>{
     console.log(`App running on port : ${PORT}`)
 })
