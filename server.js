cde73e8b9d36040e0052c56cf8ab0ffa928ea66c
richardykedang <kedangrichard@gmail.com>
2020-12-09 17:27:01 +0700

    Create document and testing the model

--- server.js
+++ server.js
@@ -10,6 +10,35 @@ mongoose.connect(DB,{
     useUnifiedTopology: true
 }).then(() => console.log("DB connection successful!"));
 
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        require: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour',tourSchema);
+
+const Testtour = new Tour({
+    name: "The Forest Hiker",
+    rating: 4.7,
+    price: 497
+});
+
+Testtour
+    .save()
+    .then(doc => console.log(doc))
+    .catch(err => console.log("ERROR : ", err))
+
 const PORT = process.env.PORT||3000;
 app.listen(3000, ()=>{
     console.log(`App running on port : ${PORT}`)
